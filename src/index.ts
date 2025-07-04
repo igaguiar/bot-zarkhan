d53633c88a268256e44fd3aef084d001eccf5f39
Igor Goldbeck de Aguiar <106772538+igaguiar@users.noreply.github.com>
2025-07-04 13:11:00 -0300

    ajustes e implementação do timer sem que a necessidade de um comando

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,42 @@
-import { bootstrap } from "#base";
+import { bootstrap, setupCreators } from "#base";
+import { ChannelType } from "discord.js";
 
-await bootstrap({ meta: import.meta });
+const { MAIN_GUILD_ID } = process.env;
+
+setupCreators({
+    commands: {
+        guilds: [MAIN_GUILD_ID!],
+    },
+});
+
+await bootstrap({
+    meta: import.meta,
+    whenReady(client) {
+        const channel = client.channels.cache.get("1390094988599361557");
+        if (channel?.type === ChannelType.GuildText) {
+            channel
+                .bulkDelete(100, true)
+                .then(() => {
+                    channel.send({
+                        content:
+                            "🧹 As mensagens anteriores foram limpas!\n🐉 Este canal foi configurado para receber os avisos do **Drogon**!",
+                    });
+                })
+                .catch(console.error);
+
+            setInterval(() => {
+                const now = new Date();
+                const minutes = now.getMinutes();
+                const currentHour = now.getHours();
+                const nextHour = (currentHour + 1) % 24;
+                const cargoId = "1390383978351034470";
+
+                if (minutes === 50 && nextHour % 2 === 0) {
+                    channel.send(
+                        `🐉 O Drogon aparecerá em 10 minutos! <@&${cargoId}>`
+                    );
+                }
+            }, 60 * 1000); // Executa a cada 1 minuto a verificação do horário
+        }
+    },
+});
--- src/settings/env.schema.ts
+++ src/settings/env.schema.ts
@@ -1,7 +1,12 @@
 import { z } from "zod";
 
 export const envSchema = z.object({
-    BOT_TOKEN: z.string({ description: "Discord Bot Token is required" }).min(1),
+    BOT_TOKEN: z
+        .string({ description: "Discord Bot Token is required" })
+        .min(1),
+    MAIN_GUILD_ID: z
+        .string({ description: "Main Guild id is required" })
+        .min(1),
     WEBHOOK_LOGS_URL: z.string().url().optional(),
     // Env vars...
 });
